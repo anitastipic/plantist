1c818490118e8a1fa762743eacf2bf54f4219f4f
anitastipic <mailforanitastipic@gmail.com>
2024-01-17 13:26:47 +0100

    set up project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,tsx}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#368966',
+      }
+    },
+  },
+  plugins: [],
+}
+
