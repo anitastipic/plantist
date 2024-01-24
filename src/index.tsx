b9855d96e44d781f82c868e08e3a0d4213451c8a
anitastipic <mailforanitastipic@gmail.com>
2024-01-24 20:21:48 +0100

    added router

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,30 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './tailwind.css';
 import './custom.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import PlantSearch from "./components/PlantSearch";
+import Navbar from "./components/Navbar";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>
+    },
+    {
+        path: "/search",
+        element: <PlantSearch/>
+    }
+])
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <Navbar/>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
