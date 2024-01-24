d375c78b890c2f8422e6419b84f6f6ae65ecad7f
anitastipic <mailforanitastipic@gmail.com>
2024-01-24 19:42:05 +0100

    added breakpoint to toggle visibility of plant image

---
+++ src/components/Home.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="h-[90vh] flex items-center justify-evenly ">
+            <div className=" w-[30vw] h-[27vw] border-[11px] rounded-2xl border-primary relative hidden md:block">
+                <img src="/plant.jpeg"
+                     className="h-[27vw] w-auto border-0 rounded-2xl absolute top-[4vh] left-[6vh]"/>
+            </div>
+            <div className="">
+                <div className="grid grid-cols-2 ">
+                    <div>
+                        <img className="h-[5vh] mt-2" src="/magnifyingGlass.png"/>
+                    </div>
+                    <div>
+                        <p className="font-logo text-[5vh]">Find</p>
+                    </div>
+                    <div>
+                        <img className="h-[5vh] mt-2" src="/plus.png"/>
+                    </div>
+                    <div>
+                        <p className="font-logo text-[5vh]">Add</p>
+                    </div>
+                    <div>
+                        <img className="h-[5vh] mt-2" src="/heart.png"/>
+                    </div>
+                    <div>
+                        <p className="font-logo text-[5vh]">Take care</p>
+                    </div>
+                </div>
+                <button
+                    className="mt-[5vh] ml-[10vh] font-logo font-bold text-xl bg-primary py-4 px-5 rounded-xl text-offWhite">GET
+                    STARTED
+                </button>
+            </div>
+        </div>
+    )
+
+}
