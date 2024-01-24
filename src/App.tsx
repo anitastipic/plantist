8f9d7927970f811c901e9882f30c655fcfdafa0b
anitastipic <mailforanitastipic@gmail.com>
2024-01-24 20:22:28 +0100

    removed navbar from app and placed it in index

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Home from "./components/Home";
 function App() {
     return (
         <div className="App bg-offWhite h-screen">
-            <Navbar/>
             <Home/>
         </div>
     )
