c0b5faab8da8654cd531b0b0b8e12833f450eddb
anitastipic <mailforanitastipic@gmail.com>
2024-01-27 00:22:17 +0100

    feat: added search by plant name through input field

--- src/Components/PlantSearch.tsx
+++ src/Components/PlantSearch.tsx
@@ -1,7 +1,53 @@
-import Navbar from "./Navbar";
+import {useEffect, useState} from "react";
+
+type Plant = {
+    latin: string
+}
 
 export default function PlantSearch() {
-    return(
-        <div></div>
+
+    const [searchInput, setSearchInput] = useState("");
+    const [plant, setPlant] = useState<Plant[]>([]);
+
+    function fetchPlant() {
+        fetch(`http://localhost:8080/plant/latin/${searchInput}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((plants) => setPlant(plants))
+            .catch((error) => console.log(error));
+
+        console.log(plant)
+    }
+
+    function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        fetchPlant();
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        event.preventDefault();
+        setSearchInput(event.target.value);
+    }
+
+    useEffect(() => {
+        fetchPlant();
+    }, [])
+
+    return (
+        <div className="bg-offWhite h-[90vh] overflow-scroll">
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="mt-[5vh] ml-[2vw] bg-primary opacity-50 w-[30vw] h-[5vh] placeholder:text-white placeholder:p-3 rounded-full"
+                    placeholder="Search plant ..."
+                    value={searchInput}
+                    onChange={handleChange}
+                />
+            </form>
+            {plant && plant.map(plantitem => (
+                <p key={plantitem.latin}>{plantitem.latin}</p>
+            ))}
+        </div>
     )
 }
