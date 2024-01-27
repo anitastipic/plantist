a93206c6576c3a3647d11281f26dc08064bd7bc1
anitastipic <mailforanitastipic@gmail.com>
2024-01-28 00:06:44 +0100

    changed nav background to body background and moved menue button to right side

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,29 +1,30 @@
 import React from "react";
-import {ReactComponent as UserIcon} from '../assets/user.svg';
 import {useNavigate} from "react-router-dom";
 
 export default function Navbar() {
 
     const navigate = useNavigate();
+
     function handleLogoClick() {
-        navigate("/home")
+        navigate("/home");
     }
 
     return (
-        <nav className="w-screen h-[10vh] px-[2vh] bg-primary flex items-center justify-between">
-            <div className="h-[10vh] w-[40vh] flex items-center justify-between">
-                <div className="h-[7vh] flex flex-col justify-evenly">
-                    <div className="h-[1.4vh] w-[6vh] bg-offWhite border-0 rounded-full"></div>
-                    <div className="h-[1.4vh] w-[6vh] bg-offWhite border-0 rounded-full"></div>
-                    <div className="h-[1.4vh] w-[6vh] bg-offWhite border-0 rounded-full"></div>
-                </div>
-                <div className="flex">
-                    <div className="font-logo text-[6vh] text-offWhite" onClick={() => handleLogoClick() }>Plantist</div>
-                    <div
-                        className="bg-offWhite h-[3.8vh] w-[6vh] ml-[12px] mt-[0.2vh] border-0 rounded-tl-[30px] rounded-br-[30px]"></div>
+        <nav className="w-screen h-[15vh] px-[2vh] bg-offWhite flex items-center justify-between">
+            <div className="h-[10vh] w-[40vh] flex justify-between items-center">
+                <div className="flex h-[15vh] p-1 pt-2">
+                    <p className="font-logo text-[9vh] text-primary" onClick={() => handleLogoClick()}>Plantist</p>
+                    <div className="bg-primary h-[3.8vh] w-[6vh] ml-[12px] mt-[0.2vh] border-0 rounded-tl-[30px] rounded-br-[30px]"></div>
                 </div>
             </div>
-            <UserIcon className="h-[9vh] w-auto"/>
+            <div className="flex items-center p-1 pt-2">
+                <img src="/userImg.png" className="h-[7vh] w-auto mr-[1.5vw]"/>
+                <div className="h-[8vh] flex flex-col justify-evenly">
+                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
+                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
+                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
+                </div>
+            </div>
         </nav>
     )
 }
