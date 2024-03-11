e20c397820b36bd2542e4219e486d868432e2aad
anitastipic <mailforanitastipic@gmail.com>
2024-03-11 12:51:12 +0100

    made Navbar responsive

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -10,20 +10,15 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="w-screen h-[15vh] px-[2vh] bg-offWhite flex items-center justify-between">
-            <div className="h-[10vh] w-[40vh] flex justify-between items-center">
-                <div className="flex h-[15vh] p-1 pt-2">
-                    <p className="font-logo text-[9vh] text-primary" onClick={() => handleLogoClick()}>Plantist</p>
-                    <div className="bg-primary h-[3.8vh] w-[6vh] ml-[12px] mt-[0.2vh] border-0 rounded-tl-[30px] rounded-br-[30px]"></div>
+        <nav className="bg-offWhite w-screen h-[15vh] px-[2vh] flex items-center justify-between ">
+            <div className="h-[15vh] w-[40vh] flex justify-between items-center">
+                <div className="flex h-[10vh] p-1">
+                    <p className="font-logo text-[4vw] text-primary relative" onClick={() => handleLogoClick()}>Plantist</p>
+                    <div className="bg-primary h-[1.8vw] w-[3vw] border-0 rounded-tl-[30px] rounded-br-[30px] absolute left-[18.5vw]"></div>
                 </div>
             </div>
-            <div className="flex items-center p-1 pt-2">
-                <img src="/userImg.png" className="h-[7vh] w-auto mr-[1.5vw]"/>
-                <div className="h-[8vh] flex flex-col justify-evenly">
-                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
-                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
-                    <div className="h-[1.4vh] w-[6vh] bg-primary border-0 rounded-full"></div>
-                </div>
+            <div className="flex items-center p-1">
+                <img src="/userImg.png" className="h-[7vh] w-auto"/>
             </div>
         </nav>
     )
