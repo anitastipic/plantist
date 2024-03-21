4d8f12839cfba5d4034189600449a2ad5c0d94ae
anitastipic <mailforanitastipic@gmail.com>
2024-03-21 12:10:29 +0100

    fixed leaf position of logo

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,16 +9,20 @@ export default function Navbar() {
         navigate("/home");
     }
 
+    function handleProfileClick() {
+        navigate("/login")
+    }
+
     return (
         <nav className="bg-offWhite w-screen h-[15vh] px-[2vh] flex items-center justify-between ">
-            <div className="h-[15vh] w-[40vh] flex justify-between items-center">
+            <div className="h-[15vh] w-[40vh] flex items-center">
                 <div className="flex h-[10vh] p-1">
-                    <p className="font-logo text-[4vw] text-primary relative" onClick={() => handleLogoClick()}>Plantist</p>
-                    <div className="bg-primary h-[1.8vw] w-[3vw] border-0 rounded-tl-[30px] rounded-br-[30px] absolute left-[18.5vw]"></div>
+                    <p className="font-logo text-[8vh] text-primary relative" onClick={() => handleLogoClick()}>Plantist</p>
+                    <div className="bg-primary h-[3.8vh] w-[6vh] border-0 rounded-tl-[30px] rounded-br-[30px] absolute left-[35vh]"></div>
                 </div>
             </div>
-            <div className="flex items-center p-1">
-                <img src="/userImg.png" className="h-[7vh] w-auto"/>
+            <div className="flex items-center">
+                <img alt="profile-icon" src="/userImg.png" className="h-auto w-[7vh]" onClick={() => handleProfileClick()}/>
             </div>
         </nav>
     )
